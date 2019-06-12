d40e6797d09f2d2ed0df7982605c0e6e1ce33049
congco <congco@foxmail>
2019-06-12 20:53:36 +0800

    feat(admin): #添加admin

---
+++ kanna-cloud/kanna-admin/src/main/java/com/congco/kannaadmin/KannaAdminApplication.java
@@ -0,0 +1,16 @@
+package com.congco.kannaadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+/**
+ * @author congco
+ */
+@SpringCloudApplication
+public class KannaAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KannaAdminApplication.class, args);
+    }
+
+}
